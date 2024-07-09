23cbb420681f1e76905e36a9fd074fbb3ac03f70
T189216 <tus0926@naver.com>
2024-07-10 01:34:28 +0900

    deploy

--- src/test/java/com/ll/md0701/Medium0701ApplicationTests.java
+++ src/test/java/com/ll/md0701/Medium0701ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.md0701;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Medium0701ApplicationTests {
 
 	@Test
